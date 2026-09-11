3f26a58d3fba8e64b62555c88c2c04435a5fa3a6
dnascvendas6-pixel <dnascvendas6@gmail.com>
2026-09-11 11:45:11 -0300

    Update route.ts

--- app/api/pix/route.ts
+++ app/api/pix/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import MercadoPagoConfig, { Payment } from 'mercadopago';
 
+
 const client = new MercadoPagoConfig({
   accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
 });
